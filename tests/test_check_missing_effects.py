9db52fc881eb1d53ba7ffac9e7fcbca813a5c7e2
Pelmen323 <Vadzim_Fiadotsyeu@epam.com>
2024-09-05 17:30:58 +0400

    Format .py test files and add documentation

--- tests/test_check_missing_effects.py
+++ tests/test_check_missing_effects.py
@@ -10,35 +10,46 @@ FALSE_POSITIVES = [
     'reseed_division_commander',
 ]
 
-# 1 Extract effects from documentation
-documentation_dict = json.load(open(path_to_documentation))
-list_with_effects_documentation = [i for i in documentation_dict['effects'].keys()]
 
-# 2 Extract effects from config files
-list_with_effects_config = []
-effect_pattern = r'alias\[effect:(.*?)\]'
-for filename in glob.iglob(path_to_config + "**/*.cwt", recursive=True):
-    with open(filename, 'r') as text_file:
-        config_file = text_file.read()
+def check_missing_effects():
+    '''
+    Validate .cwt files with effects against documentation file (script_documentation.json)\n
+    Effects are defined in .cwt files with 'alias[effect:xxx]' syntax\n
+    Script will print effects that are defined in documentation but not in .cwt files\n
+    Effects can be added to ignore by including them into FALSE_POSITIVES list
+    '''
+    # 1 Extract effects from documentation
+    documentation_dict = json.load(open(path_to_documentation))
+    list_with_effects_documentation = [i for i in documentation_dict['effects'].keys()]
 
-        if 'alias[effect:' in config_file:
-            pattern_matches = re.findall(effect_pattern, config_file)
-            if len(pattern_matches) > 0:
-                for match in pattern_matches:
-                    list_with_effects_config.append(match)
+    # 2 Extract effects from config files
+    list_with_effects_config = []
+    effect_pattern = r'alias\[effect:(.*?)\]'
+    for filename in glob.iglob(path_to_config + "**/*.cwt", recursive=True):
+        with open(filename, 'r') as text_file:
+            config_file = text_file.read()
 
-# 3 Perform a comparison
-list_with_effects_config = set(list_with_effects_config)
-results_missing_effects = [i for i in list_with_effects_documentation if i not in list_with_effects_config]
+            if 'alias[effect:' in config_file:
+                pattern_matches = re.findall(effect_pattern, config_file)
+                if len(pattern_matches) > 0:
+                    for match in pattern_matches:
+                        list_with_effects_config.append(match)
 
-raise_error = False
-if len(results_missing_effects) > 0:
-    for i in results_missing_effects:
-        if [_ for _ in FALSE_POSITIVES if _ in i] == []:
-            print(f'- [] - {i}')
-            raise_error = True
-    if raise_error:
-        raise Exception("There are effects in documentation file that are not present in .cwt files")
+    # 3 Perform a comparison
+    list_with_effects_config = set(list_with_effects_config)
+    results_missing_effects = [i for i in list_with_effects_documentation if i not in list_with_effects_config]
 
-if not raise_error:
+    raise_error = False
+    if len(results_missing_effects) > 0:
+        for i in results_missing_effects:
+            if [_ for _ in FALSE_POSITIVES if _ in i] == []:
+                print(f'- [] - {i}')
+                raise_error = True
+        if raise_error:
+            raise Exception("There are effects in documentation file that are not present in .cwt files")
+
     print("No missing effects found. Good job!")
+
+
+if __name__ == '__main__':
+    check_missing_effects()
--- tests/test_check_missing_triggers.py
+++ tests/test_check_missing_triggers.py
@@ -12,35 +12,46 @@ FALSE_POSITIVES = [
     'ideology_support_trigger',
 ]
 
-# 1 Extract triggers from documentation
-documentation_dict = json.load(open(path_to_documentation))
-list_with_triggers_documentation = [i for i in documentation_dict['triggers'].keys()]
 
-# 2 Extract triggers from config files
-list_with_triggers_config = []
-effect_pattern = r'alias\[trigger:(.*?)\]'
-for filename in glob.iglob(path_to_config + "**/*.cwt", recursive=True):
-    with open(filename, 'r') as text_file:
-        config_file = text_file.read()
+def check_missing_triggers():
+    '''
+    Validate .cwt files with triggers against documentation file (script_documentation.json)\n
+    Triggers are defined in .cwt files with 'alias[trigger:xxx]' syntax\n
+    Script will print triggers that are defined in documentation but not in .cwt files\n
+    Triggers can be added to ignore by including them into FALSE_POSITIVES list
+    '''
+    # 1 Extract triggers from documentation
+    documentation_dict = json.load(open(path_to_documentation))
+    list_with_triggers_documentation = [i for i in documentation_dict['triggers'].keys()]
 
-        if 'alias[trigger:' in config_file:
-            pattern_matches = re.findall(effect_pattern, config_file)
-            if len(pattern_matches) > 0:
-                for match in pattern_matches:
-                    list_with_triggers_config.append(match)
+    # 2 Extract triggers from config files
+    list_with_triggers_config = []
+    effect_pattern = r'alias\[trigger:(.*?)\]'
+    for filename in glob.iglob(path_to_config + "**/*.cwt", recursive=True):
+        with open(filename, 'r') as text_file:
+            config_file = text_file.read()
 
-# 3 Perform a comparison
-list_with_triggers_config = set(list_with_triggers_config)
-results_missing_triggers = [i for i in list_with_triggers_documentation if i not in list_with_triggers_config]
+            if 'alias[trigger:' in config_file:
+                pattern_matches = re.findall(effect_pattern, config_file)
+                if len(pattern_matches) > 0:
+                    for match in pattern_matches:
+                        list_with_triggers_config.append(match)
 
-raise_error = False
-if len(results_missing_triggers) > 0:
-    for i in results_missing_triggers:
-        if [_ for _ in FALSE_POSITIVES if _ in i] == []:
-            print(f'- [] - {i}')
-            raise_error = True
-    if raise_error:
-        raise Exception("There are triggers in documentation file that are not present in .cwt files")
+    # 3 Perform a comparison
+    list_with_triggers_config = set(list_with_triggers_config)
+    results_missing_triggers = [i for i in list_with_triggers_documentation if i not in list_with_triggers_config]
 
-if not raise_error:
+    raise_error = False
+    if len(results_missing_triggers) > 0:
+        for i in results_missing_triggers:
+            if [_ for _ in FALSE_POSITIVES if _ in i] == []:
+                print(f'- [] - {i}')
+                raise_error = True
+        if raise_error:
+            raise Exception("There are triggers in documentation file that are not present in .cwt files")
+
     print("No missing triggers found. Good job!")
+
+
+if __name__ == '__main__':
+    check_missing_triggers()
